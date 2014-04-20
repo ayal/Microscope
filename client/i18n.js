874a169582dd3a727bc93dced2ec477373b8c2e7
ayal <ayalgelles@gmail.com>
2014-04-20 16:42:17 +0300

    Update i18n.js

--- client/i18n.js
+++ client/i18n.js
@@ -1,7 +1,13 @@
 i18n.map('he', {
    'discuss': 'תגובות',
-   'comment': 'הערות',
+   'comment': 'תגובות',
    'like': 'טוב',
    'subby': 'נכתב ע"י',
+   'comonthis': 'תגובות לפוסט הזה',
+   'addcomment': 'הוסף תגובה',
+   'appname': 'nuTalk',
+   'new': 'חדשים',
+   'best': 'טובים',
+   'subpost': 'לכתוב פוסט'
    
 });
